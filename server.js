2a18a096cbf17bd1e0b83931b4abbf3270a358d4
atulgade-iprogrammer <atulg@iprogrammer.co>
2022-11-23 12:27:02 +0530

    Updated Commit

--- server.js
+++ server.js
@@ -1,11 +1,22 @@
-const  express = require('express');
-const path=require('path');
-const app=express();
+// const  express = require('express');
+// const path=require('path');
+// const app=express();
 
 
 
-app.use(express.static('./dist/eCommerceWebsite'));
-app.get('/*',(req,res)=>
-res.sendFile('index.html',{root:'dist/angular-heroku/'}));
+// app.use(express.static('./dist/eCommerceWebsite'));
+// app.get('/*',(req,res)=>
+// {
+//   res.sendFile(path.join(
+//   '/dist/eCommerceWebsite/index.html'));});
+// app.listen(process.env.PORT || 8080);
 
+
+const express = require('express');
+const path = require('path');
+const app = express();
+app.use(express.static(__dirname + '/dist/eCommerceWebsite'));
+app.get('/*', function(req,res) {
+res.sendFile(path.join(__dirname+
+'/dist/eCommerceWebsite/index.html'));});
 app.listen(process.env.PORT || 8080);
